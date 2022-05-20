eba76939217e760b2056f142f20bcad70ffa41fe
salmenhajji <salmen.hj@gmail.com>
2022-05-20 14:18:39 +0100

    33

--- src/components/pages/Ariana.js
+++ src/components/pages/Ariana.js
@@ -7,9 +7,28 @@ import CardItem from '../CardItem';
 function Ariana() {
     return (
       <div className='cards'>
-        <h1>!</h1>
+        <h1>Check out these your Destinations!</h1>
         <div className='cards__container'>
-  
+          <div className='cards__wrapper'>
+            <ul className='cards__items'>
+              <CardItem
+                src='https://i.pinimg.com/564x/a6/0e/d0/a60ed0eb0b71c5c142c3120f5163553b.jpg'
+                text='Tunis --> Manouba'
+                label='Tunis'
+                path='/services'
+              />
+              <CardItem
+                src='https://www.middleeasteye.net/sites/default/files/styles/article_page/public/images-story/Ouv.png?itok=96NzZg1j'
+                text='Tunis --> Ariana'
+                label='Manouba'
+                path='/services'
+              />
+       
+              
+            </ul>
+    
+     
+          </div>
         </div>
       </div>
     );
